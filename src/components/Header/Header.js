6b4da496d358e8fd9016b27cbf89a7e1ddfccd38
jwhong <jwhong98@gmail.com>
2022-02-08 17:11:58 -0800

    header completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classes from "./Header.module.css";
+import HeaderForm from "./HeaderForm";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <section className={classes.header}>
+      <h2>IP Address Tracker</h2>
+      <HeaderForm />
+    </section>
+  );
 };
 
 export default Header;
--- src/components/Header/HeaderForm.js
+++ src/components/Header/HeaderForm.js
@@ -1,7 +1,19 @@
 import React from "react";
+import classes from "./HeaderForm.module.css";
+import arrow from "../../images/icon-arrow.svg";
 
 const HeaderForm = () => {
-  return <div></div>;
+  return (
+    <form className={classes.form}>
+      <input
+        className={classes.input}
+        placeholder="Search for any IP address or domain"
+      />
+      <button>
+        <img src={arrow} alt="arrow" />
+      </button>
+    </form>
+  );
 };
 
 export default HeaderForm;
